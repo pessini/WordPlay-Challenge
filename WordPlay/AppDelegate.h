a0333716844a0b3a14b2c97580425f23f08164b1
Leandro Pessini <pessini@Leandros-MacBook-Air.local>
2015-03-10 16:13:02 -0300

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Leandro Pessini on 3/10/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
